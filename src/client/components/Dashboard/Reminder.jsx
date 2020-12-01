bba3115072ebd2c6d8eba2516dd1f99e14e77e22
Amanda Parrott <amandaparrott@amandas-air.attlocal.net>
2020-12-01 16:50:02 -0600

    started reminder component

--- src/client/components/Dashboard/Reminder.jsx
+++ src/client/components/Dashboard/Reminder.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Reminder = () => {
+    const [reminder, setReminder] = useState<string>("");
 
+    const handleAdd = (e) => {
+        e.preventDefault();
+        newReminder();
+    }
+
+    const newReminder = async () => {
+        const reminder = { 
+            content: content 
+        };
+        let res = await fetch("/api/reminders", {
+            method: 'POST',
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+                body: JSON.stringify{reminder},
+        })
+        if (res.ok) {
+            console.log('reminder posted');
+            history.push('/');
+        } else {
+            console.log('reminder not posted');
+        }
+    }
+    return (
+        
+    )
+    
 }
 
 export default Reminder;
